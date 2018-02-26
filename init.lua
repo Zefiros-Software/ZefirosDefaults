d9d31a4b3537d60447b5e77aeaa30e17597c0c40
PaulVisscher <paul.pev@hotmail.com>
2018-02-26 22:49:20 +0100

    Fix astyle paths

--- init.lua
+++ init.lua
@@ -512,8 +512,8 @@ zpm.newaction {
         local astylerc = path.join(_MAIN_SCRIPT_DIR, ".astylerc")
         zpm.util.writeAll(astylerc, zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/.astylerc")))
         local dir = path.join(_MAIN_SCRIPT_DIR, zefiros.env.projectDirectory())
-        os.executef("astyle --options=%s --recursive --exclude=%s/extern/**  %s/*.cpp  %s/*.h", astylerc, _MAIN_SCRIPT_DIR, dir, dir)
-        os.executef("astyle --options=%s  --recursive --exclude=%s/test/extern/**  %s/test/*.cpp  %s/test/*.h", astylerc, _MAIN_SCRIPT_DIR, _MAIN_SCRIPT_DIR, _MAIN_SCRIPT_DIR)
+        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.cpp\"  \"%s/*.h\"", astylerc, dir, dir)
+        os.executef("astyle --options=%s  --recursive -i --exclude=\"test/extern\"  \"%s/test/*.cpp\"  \"%s/test/*.h\"", astylerc, _MAIN_SCRIPT_DIR, _MAIN_SCRIPT_DIR)
         
         local olicense = string.format("extensions: .h .cpp .cc .hpp\n/**\n * %s\n */", zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/mit.tmpl")):gsub("${years}", "%%CurrentYear%%"):gsub("${owner}", "Zefiros Software"):gsub("\n", "\n * "))
         local license = ""
@@ -565,8 +565,8 @@ zpm.newaction {
         local astylerc = path.join(root, ".astylerc")
         zpm.util.writeAll(astylerc, zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/.astylerc")))
         local dir = path.join(root, zefiros.env.projectDirectory())
-        os.executef("astyle --options=%s --recursive --exclude=%s/extern/**  %s/*.cpp  %s/*.h", astylerc, dir, dir, dir)
-        os.executef("astyle --options=%s  --recursive --exclude=%s/test/extern/** %s/test/*.cpp  %s/test/*.h", astylerc, root, root, root)
+        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.cpp\"  \"%s/*.h\"", astylerc, dir, dir)
+        os.executef("astyle --options=%s  --recursive -i --exclude=\"test/extern\" \"%s/test/*.cpp\"  \"%s/test/*.h\"", astylerc, root, root)
         
         local olicense = string.format("extensions: .h .cpp .cc .hpp\n/**\n * %s\n */", zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/mit.tmpl")):gsub("${years}", "%%CurrentYear%%"):gsub("${owner}", "Zefiros Software"):gsub("\n", "\n * "))
         local license = ""
