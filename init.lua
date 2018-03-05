a639f44cf7e87e36ea576c661008466aad9a3169
PaulVisscher <paul.pev@hotmail.com>
2018-03-05 20:15:15 +0100

    Update astyle

--- init.lua
+++ init.lua
@@ -512,7 +512,8 @@ zpm.newaction {
         local astylerc = path.join(_MAIN_SCRIPT_DIR, ".astylerc")
         zpm.util.writeAll(astylerc, zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/.astylerc")))
         local dir = path.join(_MAIN_SCRIPT_DIR, zefiros.env.projectDirectory())
-        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.cpp\"  \"%s/*.h\"", astylerc, dir, dir)
+        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.cpp\"", astylerc, dir)
+        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.h\"", astylerc, dir)
         os.executef("astyle --options=%s  --recursive -i --exclude=\"test/extern\"  \"%s/test/*.cpp\"  \"%s/test/*.h\"", astylerc, _MAIN_SCRIPT_DIR, _MAIN_SCRIPT_DIR)
         
         local olicense = string.format("extensions: .h .cpp .cc .hpp\n/**\n * %s\n */", zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/mit.tmpl")):gsub("${years}", "%%CurrentYear%%"):gsub("${owner}", "Zefiros Software"):gsub("\n", "\n * "))
@@ -565,7 +566,8 @@ zpm.newaction {
         local astylerc = path.join(root, ".astylerc")
         zpm.util.writeAll(astylerc, zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/.astylerc")))
         local dir = path.join(root, zefiros.env.projectDirectory())
-        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.cpp\"  \"%s/*.h\"", astylerc, dir, dir)
+        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.cpp\"", astylerc, dir)
+        os.executef("astyle --options=%s --recursive -i --exclude=\"extern\"  \"%s/*.h\"", astylerc, dir)
         os.executef("astyle --options=%s  --recursive -i --exclude=\"test/extern\" \"%stest/*.cpp\"  \"%stest/*.h\"", astylerc, root, root)
         
         local olicense = string.format("extensions: .h .cpp .cc .hpp\n/**\n * %s\n */", zpm.util.readAll(path.join(zpm.env.getScriptPath(), "templates/mit.tmpl")):gsub("${years}", "%%CurrentYear%%"):gsub("${owner}", "Zefiros Software"):gsub("\n", "\n * "))
